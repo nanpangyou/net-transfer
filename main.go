5e31970f5a8263dbe8ab2e1fc45102a6aaced29f
nanpangyou <yigehd@foxmail.com>
2023-02-23 10:57:46 +0800

    添加ui

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
+//go:embed frontend/dist/*
+var FS embed.FS
+
 func main() {
+
+	s := func() {
+		fmt.Print("xxx")
+	}
+	s()
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		r := gin.Default()
-		r.GET("/", func(c *gin.Context) {
-			// c.JSON(200, gin.H{
-			// 	"message": "pong1",
-			// })
-			c.Writer.Write([]byte("adfa"))
+		// 已/sataic开头的路径，都会去读取frontend/dist目录下的文件
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		// 静态的404的路由默认返回index.html
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
+
 		r.Run() // listen and serve on 0.0.0.0:8080
 	}()
 
 	var ui lorca.UI
-	ui, _ = lorca.New("http://localhost:8080/", "", 800, 600, "--disable-sync", "--disable-translate")
+	ui, _ = lorca.New("http://localhost:8080/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate")
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
 	select {
@@ -31,4 +61,5 @@ func main() {
 	case <-chSignal:
 	}
 	ui.Close()
+
 }
